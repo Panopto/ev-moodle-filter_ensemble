ada32e85f32f4e8233fe4d351321958948f00a38
Jim Pease <jmpease@codeserv.us>
2018-12-17 16:50:00 -0500

    Updated version for Moodle 3.6 dev.

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-// You are on the MOODLE_35_STABLE branch.  Do NOT update the version
+// You are on the MOODLE_36_STABLE branch.  Do NOT update the version
 // branching date rather update the release increment.
-$plugin->version        = 2018060601;
-$plugin->release        = '3.5 (Build: 2018060601)';
+$plugin->version        = 2018121700;
+$plugin->release        = '3.6 (Build: 2018121700)';
 $plugin->maturity       = MATURITY_STABLE;
-$plugin->requires       = 2018051700;
+$plugin->requires       = 2018120300;
 $plugin->component      = 'filter_ensemble';
